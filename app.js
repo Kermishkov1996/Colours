3b55c8501f794b3a26a529f12510f5bec6599272
Kermishkov1996 <gtytr.n@yandex.ru>
2024-04-04 15:18:51 +0400

    add setTextColor

--- app.js
+++ app.js
@@ -13,11 +13,22 @@ function generateRandomColor() {
 function setRandomColors() {
 	cols.forEach(col =>  {
 		const text = col.querySelector('h2')
-		const color = generateRandomColor()
+		const button = col.querySelector('button')
+		// const color = generateRandomColor()
+		const color = chroma.random() //библиотека chroma.js
 
 		text.textContent = color
 		col.style.background = color
+
+		setTextColor(text, color)
+		setTextColor(button, color)
 	})
+}
+
+function setTextColor(text, color) {
+	const luminance = chroma(color).luminance()
+
+	text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
