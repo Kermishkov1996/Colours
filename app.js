5c4bd7eda7949c3165e5b7d661b91f8775b14e55
Kermishkov1996 <gtytr.n@yandex.ru>
2024-04-04 15:07:10 +0400

    change text to color

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const cols = document.querySelectorAll('.col');
 
 function generateRandomColor() {
-	const hexCodes = '0123456789ABCDEF';
-	let color = '';
+	const hexCodes = '0123456789ABCDEF'
+	let color = ''
 
 	for (let i = 0; i < 6; i++) {
 		color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
@@ -12,7 +12,11 @@ function generateRandomColor() {
 
 function setRandomColors() {
 	cols.forEach(col =>  {
-		col.style.background = generateRandomColor()
+		const text = col.querySelector('h2')
+		const color = generateRandomColor()
+
+		text.textContent = color
+		col.style.background = color
 	})
 }
 
